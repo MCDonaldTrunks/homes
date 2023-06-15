8dcb9fe3c8ca1229a99bec45f92c3b859be0b2e1
MCDonaldTrunks <luis_p456@outlook.com>
2023-06-15 14:41:43 -0700

    chat li bug on sidebar

--- src/app/User/components/SidebarComponent.jsx
+++ src/app/User/components/SidebarComponent.jsx
@@ -5,7 +5,7 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import styles from "./SidebarComponent.module.scss";
 
- 
+
 
 
 function SidebarComponent(props) {
@@ -36,15 +36,16 @@ function SidebarComponent(props) {
           <MenuItem> Profile </MenuItem>
           <MenuItem> Settings </MenuItem>
         </SubMenu>
-        
-          <MenuItem>
-            <a>
-              <Link className={styles.barLinks} href="/User/UserChat">
-                Messenger
-              </Link>
-            </a>
-          </MenuItem>
-        
+
+        {/*use classes from MenutItem on <li> tag and Link component.*/}
+        <li className="ps-menuitem-root css-1t8x7v1">
+
+          <Link  className={styles.barLinks + ' ps-menu-button'} href="/User/UserChat">
+            Messenger
+          </Link>
+
+        </li>
+
         <MenuItem> Upload</MenuItem>
         <MenuItem> Calendar </MenuItem>
       </Menu>
