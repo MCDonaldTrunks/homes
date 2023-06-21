9e6e0dd30274453711e814e634a1cae0e2e7df7b
MCDonaldtrunks <luis_p456@outlook.com>
2023-06-21 04:05:11 -0700

    solved npm i permission problem

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Header from './rootComponents/Header'
+import Login from './Login/page'
 
 export default function Home() {
+
+  const loged = true;
+
   return (
-    <main className={styles.main}>
-      
-    </main>
+
+    
+
+  <main className={styles.main}>
+      {loged? <Login></Login>: <p>logged in</p>}
+  </main>
+
+
   )
 }
